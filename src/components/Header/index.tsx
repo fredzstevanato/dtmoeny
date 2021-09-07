4c41cdc28e7e1bd560938a8f47be99fdfe2e9b59
Frederico Stevanato Rocha <fredzstevanato@gmail.com>
2021-09-07 18:33:46 -0300

    refactor: modal and pass function child component to parent component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
 import logoImg from '../../assets/logo.svg';
 
-import { Container, Content } from './sytles'
+import { Container, Content } from './sytles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt Money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>Nova transação</button>
       </Content>
     </Container>
   )
