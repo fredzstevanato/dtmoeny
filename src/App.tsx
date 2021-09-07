4ea2d6a6c32e02f2fc7a1104e52b0c25f1cafc67
Frederico Stevanato Rocha <fredzstevanato@gmail.com>
2021-09-07 18:44:44 -0300

    refactor: add NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Modal from 'react-modal';
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
@@ -42,14 +43,7 @@ export function App() {
     <>  
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
-
-      <Modal
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-            <h2>Cadastrar Transações</h2>
-        </Modal>
-
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
     </>
   );
