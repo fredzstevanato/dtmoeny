6758b6efd481df59d4453d2f22014f6ae0240439
Frederico Stevanato Rocha <fredzstevanato@gmail.com>
2021-09-06 22:36:00 -0300

    feat: add component Dashboard

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from '../Summary'
+import { Container } from './sytles'
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  )
+}
