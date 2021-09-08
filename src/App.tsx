9a8152e993c6b1a5244818ac29d9d0104b545a11
Frederico Stevanato Rocha <fredzstevanato@gmail.com>
2021-09-07 22:11:23 -0300

    fix: refact miragejs in App for index

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { createServer } from "miragejs";
 import { useState } from "react";
 
 import Modal from 'react-modal';
@@ -9,25 +8,6 @@ import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
-
-createServer({
-  routes() {
-    this.namespace = "api";
-
-    this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          catogory: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
-  }
-})
 
 export function App() {
   const [ isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false);
