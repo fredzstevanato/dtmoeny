bec2e52fc0b4c88498b2498b2154a26bf65f8227
Frederico Stevanato Rocha <fredzstevanato@gmail.com>
2021-09-13 11:26:48 -0300

    feat: add TransactionsContext

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -20,12 +21,12 @@ export function App() {
     setIsNewTransactionModalOpen(false)
   }
   return (
-    <>  
+    <TransactionsProvider>  
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
